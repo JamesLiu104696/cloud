f8913ff8b8b047ee7112728b12d221d7482c16c2
JamesLiu104696 <lgq_104696@163.com>
2019-04-21 22:07:55 +0800

    2222

--- cloud/srv1/src/main/java/com/starbar/srv1/Service1Application.java
+++ cloud/srv1/src/main/java/com/starbar/srv1/Service1Application.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@org.springframework.cloud.openfeign.EnableFeignClients
+@EnableFeignClients
 @EnableDiscoveryClient
 public class Service1Application {
 	public static void main(String[] args){
