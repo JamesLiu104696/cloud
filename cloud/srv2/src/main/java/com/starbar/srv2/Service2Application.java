a7023b478dd2dc598850a359496c4e587902fde8
JamesLiu104696 <lgq_104696@163.com>
2019-04-22 21:35:10 +0800

    config

--- cloud/srv2/src/main/java/com/starbar/srv2/Service2Application.java
+++ cloud/srv2/src/main/java/com/starbar/srv2/Service2Application.java
@@ -2,12 +2,10 @@ package com.starbar.srv2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
 public class Service2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Service2Application.class, args);
